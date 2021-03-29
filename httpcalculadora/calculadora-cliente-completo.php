60223ec6fe7ceed51e86ccc680c1dbd2550eb42a
CristianYesca <cristianyesca@gmail.com>
2021-03-28 23:36:40 -0500

    calculadora

---
+++ httpcalculadora/calculadora-cliente-completo.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Prog4</title>
+</head>
+<body>
+    <form action="calculadora-cliente-completo.html" method="POST">
+        N1: <input name="n1"/> <br/>
+        N2: <input name="n2"/> <br/>
+        Resultado: <php echo $suma; ?>
+        <input type="submit" value="Sumar"/>
+    </form>
+    
+</body>
+</html>
